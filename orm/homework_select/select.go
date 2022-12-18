048d0f64d1c6cc4fe368d5b47854cbe91cf9e67e
Jacky CSL <jcslmu@gmail.com>
2022-12-18 09:53:06 +0800

    implement group by, having, order by, offset limit

--- orm/homework_select/select.go
+++ orm/homework_select/select.go
@@ -1,48 +1,298 @@
 package orm
 
+import (
+	"strings"
+
+	"github.com/jackycsl/geektime-go-practical/orm/homework_select/internal/errs"
+	"github.com/jackycsl/geektime-go-practical/orm/homework_select/model"
+)
+
 // Selector 用于构造 SELECT 语句
 type Selector[T any] struct {
-	db *DB
+	model *model.Model
+	db    *DB
+
+	sb     strings.Builder
+	args   []any
+	table  string
+	where  []Predicate
+	having []Predicate
+
+	columns []Selectable
+	groupBy []Column
+	orderBy []OrderBy
+	offset  int
+	limit   int
 }
 
-func (s *Selector[T]) Select(cols...Selectable) *Selector[T] {
-	panic("implement me")
+func (s *Selector[T]) Select(cols ...Selectable) *Selector[T] {
+	s.columns = cols
+	return s
 }
 
 // From 指定表名，如果是空字符串，那么将会使用默认表名
 func (s *Selector[T]) From(tbl string) *Selector[T] {
-	panic("implement me")
+	s.table = tbl
+	return s
 }
 
 func (s *Selector[T]) Build() (*Query, error) {
-	panic("implement me")
+	var (
+		t   T
+		err error
+	)
+	s.model, err = s.db.r.Get(&t)
+	if err != nil {
+		return nil, err
+	}
+
+	s.sb.WriteString("SELECT ")
+	if err = s.buildColumns(); err != nil {
+		return nil, err
+	}
+	s.sb.WriteString(" FROM ")
+	if s.table == "" {
+		s.sb.WriteByte('`')
+		s.sb.WriteString(s.model.TableName)
+		s.sb.WriteByte('`')
+	} else {
+		s.sb.WriteString(s.table)
+	}
+
+	if len(s.where) > 0 {
+		s.sb.WriteString(" WHERE ")
+		if err = s.buildPredicates(s.where); err != nil {
+			return nil, err
+		}
+	}
+
+	if len(s.groupBy) > 0 {
+		s.sb.WriteString(" GROUP BY ")
+		for i, c := range s.groupBy {
+			if i > 0 {
+				s.sb.WriteByte(',')
+			}
+			if err = s.buildColumn(c.name, ""); err != nil {
+				return nil, err
+			}
+		}
+	}
+
+	if len(s.having) > 0 {
+		s.sb.WriteString(" HAVING ")
+		if err = s.buildPredicates(s.having); err != nil {
+			return nil, err
+		}
+	}
+
+	if len(s.orderBy) > 0 {
+		s.sb.WriteString(" ORDER BY ")
+		if err = s.buildOrderBy(); err != nil {
+			return nil, err
+		}
+	}
+
+	if s.limit > 0 {
+		s.sb.WriteString(" LIMIT ?")
+		s.addArgs(s.limit)
+	}
+
+	if s.offset > 0 {
+		s.sb.WriteString(" OFFSET ?")
+		s.addArgs(s.offset)
+	}
+
+	s.sb.WriteString(";")
+	return &Query{
+		SQL:  s.sb.String(),
+		Args: s.args,
+	}, nil
 }
 
+func (s *Selector[T]) buildColumns() error {
+	if len(s.columns) == 0 {
+		// 没有指定列
+		s.sb.WriteByte('*')
+		return nil
+	}
+
+	for i, c := range s.columns {
+		if i > 0 {
+			s.sb.WriteByte(',')
+		}
+		switch val := c.(type) {
+		case Column:
+			if err := s.buildColumn(val.name, val.alias); err != nil {
+				return nil
+			}
+		case Aggregate:
+			if err := s.buildAggregate(val, true); err != nil {
+				return nil
+			}
+		case RawExpr:
+			s.sb.WriteString(val.raw)
+			if len(val.args) != 0 {
+				s.addArgs(val.args...)
+			}
+		default:
+			return errs.NewErrUnsupportedSelectable(c)
+		}
+	}
+
+	return nil
+}
+
+func (s *Selector[T]) buildColumn(c string, alias string) error {
+	s.sb.WriteByte('`')
+	fd, ok := s.model.FieldMap[c]
+	if !ok {
+		return errs.NewErrUnknownField(c)
+	}
+	s.sb.WriteString(fd.ColName)
+	s.sb.WriteByte('`')
+	if alias != "" {
+		s.buildAs(alias)
+	}
+	return nil
+}
+
+func (s *Selector[T]) buildAs(alias string) {
+	if alias != "" {
+		s.sb.WriteString(" AS ")
+		s.sb.WriteByte('`')
+		s.sb.WriteString(alias)
+		s.sb.WriteByte('`')
+	}
+}
+
+func (s *Selector[T]) addArgs(args ...any) {
+	if s.args == nil {
+		s.args = make([]any, 0, 8)
+	}
+	s.args = append(s.args, args...)
+}
+
+func (s *Selector[T]) buildAggregate(a Aggregate, useAlias bool) error {
+	s.sb.WriteString(a.fn)
+	s.sb.WriteString("(`")
+	fd, ok := s.model.FieldMap[a.arg]
+	if !ok {
+		return errs.NewErrUnknownField(a.arg)
+	}
+	s.sb.WriteString(fd.ColName)
+	s.sb.WriteString("`)")
+	if useAlias {
+		s.buildAs(a.alias)
+	}
+	return nil
+}
+
+func (s *Selector[T]) buildPredicates(ps []Predicate) error {
+	p := ps[0]
+	for i := 1; i < len(ps); i++ {
+		p = p.And(ps[i])
+	}
+	return s.buildExpression(p)
+}
+
+func (s *Selector[T]) buildExpression(e Expression) error {
+	if e == nil {
+		return nil
+	}
+	switch exp := e.(type) {
+	case Column:
+		return s.buildColumn(exp.name, "")
+	case Aggregate:
+		return s.buildAggregate(exp, false)
+	case value:
+		s.sb.WriteByte('?')
+		s.addArgs(exp.val)
+	case RawExpr:
+		s.sb.WriteString(exp.raw)
+		if len(exp.args) != 0 {
+			s.addArgs(exp.args...)
+		}
+	case Predicate:
+		_, lp := exp.left.(Predicate)
+		if lp {
+			s.sb.WriteByte('(')
+		}
+		if err := s.buildExpression(exp.left); err != nil {
+			return err
+		}
+		if lp {
+			s.sb.WriteByte(')')
+		}
+
+		if exp.op == "" {
+			return nil
+		}
+
+		s.sb.WriteByte(' ')
+		s.sb.WriteString(exp.op.String())
+		s.sb.WriteByte(' ')
+
+		_, rp := exp.right.(Predicate)
+		if rp {
+			s.sb.WriteByte('(')
+		}
+		if err := s.buildExpression(exp.right); err != nil {
+			return err
+		}
+		if rp {
+			s.sb.WriteByte(')')
+		}
+	default:
+		return errs.NewErrUnsupportedExpressionType(exp)
+	}
+	return nil
+}
+
+func (s *Selector[T]) buildOrderBy() error {
+	for idx, ob := range s.orderBy {
+		if idx > 0 {
+			s.sb.WriteByte(',')
+		}
+		err := s.buildColumn(ob.col, "")
+		if err != nil {
+			return err
+		}
+		s.sb.WriteByte(' ')
+		s.sb.WriteString(ob.order)
+	}
+	return nil
+}
 
 // Where 用于构造 WHERE 查询条件。如果 ps 长度为 0，那么不会构造 WHERE 部分
 func (s *Selector[T]) Where(ps ...Predicate) *Selector[T] {
-	panic("implement me")
+	s.where = ps
+	return s
 }
 
 // GroupBy 设置 group by 子句
-func (s *Selector[T]) GroupBy(cols...Column) *Selector[T] {
-	panic("implement me")
+func (s *Selector[T]) GroupBy(cols ...Column) *Selector[T] {
+	s.groupBy = cols
+	return s
 }
 
 func (s *Selector[T]) Having(ps ...Predicate) *Selector[T] {
-	panic("implement me")
+	s.having = ps
+	return s
 }
 
 func (s *Selector[T]) Offset(offset int) *Selector[T] {
-	panic("implement me")
+	s.offset = offset
+	return s
 }
 
 func (s *Selector[T]) Limit(limit int) *Selector[T] {
-	panic("implement me")
+	s.limit = limit
+	return s
 }
 
-func (s *Selector[T]) OrderBy(orderBys...OrderBy) *Selector[T] {
-	panic("implement me")
+func (s *Selector[T]) OrderBy(orderBys ...OrderBy) *Selector[T] {
+	s.orderBy = orderBys
+	return s
 }
 
 func NewSelector[T any](db *DB) *Selector[T] {
@@ -56,12 +306,20 @@ type Selectable interface {
 }
 
 type OrderBy struct {
+	col   string
+	order string
 }
 
 func Asc(col string) OrderBy {
-	panic("implement me")
+	return OrderBy{
+		col:   col,
+		order: "ASC",
+	}
 }
 
 func Desc(col string) OrderBy {
-	panic("implement me")
+	return OrderBy{
+		col:   col,
+		order: "DESC",
+	}
 }
