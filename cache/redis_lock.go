74199661580d2e0e6b011c380ee439fd5cd2fe79
Jacky CSL <jcslmu@gmail.com>
2023-01-17 10:26:31 +0800

    [cache]redis lock and unit test

---
+++ cache/redis_lock.go
@@ -0,0 +1,102 @@
+package cache
+
+import (
+	"context"
+	"errors"
+	"time"
+
+	_ "embed"
+
+	redis "github.com/go-redis/redis/v9"
+	"github.com/google/uuid"
+)
+
+var (
+	ErrFailedToPreemptLock = errors.New("redis-lock: 抢锁失败")
+	ErrLockNotHold         = errors.New("redis-lock: 你没有持有锁")
+
+	//go:embed lua/unlock.lua
+	luaUnlock string
+
+	//go:embed lua/refresh.lua
+	luaRefresh string
+
+	//go:embed lua/lock.lua
+	luaLock string
+)
+
+// Client 就是对 redis.Cmdable 的二次封装
+type Client struct {
+	client redis.Cmdable
+}
+
+func NewClient(client redis.Cmdable) *Client {
+	return &Client{
+		client: client,
+	}
+}
+
+func (c *Client) TryLock(ctx context.Context, key string, expiration time.Duration) (*Lock, error) {
+	val := uuid.New().String()
+	ok, err := c.client.SetNX(ctx, key, val, expiration).Result()
+	if err != nil {
+		return nil, err
+	}
+	if !ok {
+		// 代表的是别人抢到了锁
+		return nil, ErrFailedToPreemptLock
+	}
+	return &Lock{
+		client: c.client,
+		key:    key,
+		value:  val,
+	}, nil
+}
+
+// func (c *Client) Unlock(ctx context.Context, lock *Lock) error {
+// }
+
+type Lock struct {
+	client redis.Cmdable
+	key    string
+	value  string
+}
+
+func (l *Lock) Unlock(ctx context.Context) error {
+	res, err := l.client.Eval(ctx, luaUnlock, []string{l.key}, l.value).Int64()
+	// if err == redis.Nil {
+	// 	return ErrLockNotHold
+	// }
+	if err != nil {
+		return err
+	}
+	if res != 1 {
+		return ErrLockNotHold
+	}
+	return nil
+}
+
+// func (l *Lock) Unlock(ctx context.Context) error {
+// 	// 我现在要先判断一下，这把锁是不是我的锁
+// 	val, err := l.client.Get(ctx, l.key).Result()
+// 	if err != nil {
+// 		return err
+// 	}
+// 	if val != l.value {
+// 		return errors.New("锁不是自己的锁")
+// 	}
+
+// 	// 在这个地方，键值对被人删了，紧接着另外一个实例加锁
+// 	// 把键值对删掉
+// 	cnt, err := l.client.Del(ctx, l.key).Result()
+// 	if err != nil {
+// 		return err
+// 	}
+// 	if cnt != 1 {
+// 		// 代表你加的锁，过期了
+// 		// log.Info("redis-lock: 解锁失败，锁不存在")
+// 		// return nil
+// 		return ErrLockNotHold
+// 	}
+// 	return nil
+// }
---
+++ cache/redis_lock_test.go
@@ -0,0 +1,152 @@
+package cache
+
+import (
+	"context"
+	"testing"
+	"time"
+
+	"github.com/go-redis/redis/v9"
+	"github.com/golang/mock/gomock"
+	"github.com/jackycsl/geektime-go-practical/cache/mocks"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestClient_Lock(t *testing.T) {
+	testCases := []struct {
+		name string
+		mock func(*gomock.Controller) redis.Cmdable
+
+		key string
+
+		wantErr  error
+		wantLock *Lock
+	}{
+		{
+			name: "set nx error",
+			mock: func(ctrl *gomock.Controller) redis.Cmdable {
+				cmd := mocks.NewMockCmdable(ctrl)
+				res := redis.NewBoolResult(false, context.DeadlineExceeded)
+				cmd.EXPECT().SetNX(context.Background(), "key1", gomock.Any(), time.Minute).Return(res)
+				return cmd
+			},
+			key:     "key1",
+			wantErr: context.DeadlineExceeded,
+		},
+		{
+			name: "failed to preempt lock",
+			mock: func(ctrl *gomock.Controller) redis.Cmdable {
+				cmd := mocks.NewMockCmdable(ctrl)
+				res := redis.NewBoolResult(false, nil)
+				cmd.EXPECT().SetNX(context.Background(), "key1", gomock.Any(), time.Minute).Return(res)
+				return cmd
+			},
+			key:     "key1",
+			wantErr: ErrFailedToPreemptLock,
+		},
+		{
+			name: "locked",
+			mock: func(ctrl *gomock.Controller) redis.Cmdable {
+				cmd := mocks.NewMockCmdable(ctrl)
+				res := redis.NewBoolResult(true, nil)
+				cmd.EXPECT().SetNX(context.Background(), "key1", gomock.Any(), time.Minute).Return(res)
+				return cmd
+			},
+			key: "key1",
+			wantLock: &Lock{
+				key: "key1",
+			},
+		},
+	}
+	for _, tc := range testCases {
+		t.Run(tc.name, func(t *testing.T) {
+			ctrl := gomock.NewController(t)
+			defer ctrl.Finish()
+			client := NewClient(tc.mock(ctrl))
+			l, err := client.TryLock(context.Background(), tc.key, time.Minute)
+			assert.Equal(t, tc.wantErr, err)
+			if err != nil {
+				return
+			}
+			assert.Equal(t, tc.wantLock.key, l.key)
+			// 赋予值了
+			assert.NotEmpty(t, l.value)
+		})
+	}
+}
+
+func TestLock_Unlock(t *testing.T) {
+	// ctrl := gomock.NewController()
+	// defer ctrl.Finish()
+	testCases := []struct {
+		name  string
+		mock  func(ctrl *gomock.Controller) redis.Cmdable
+		key   string
+		value string
+
+		lock    *Lock
+		wantErr error
+	}{
+		{
+			name: "eval error",
+			mock: func(ctrl *gomock.Controller) redis.Cmdable {
+				cmd := mocks.NewMockCmdable(ctrl)
+				res := redis.NewCmd(context.Background())
+				res.SetErr(context.DeadlineExceeded)
+				cmd.EXPECT().Eval(context.Background(), luaUnlock, []string{"key1"}, []any{"value1"}).
+					Return(res)
+				return cmd
+			},
+			key:     "key1",
+			value:   "value1",
+			wantErr: context.DeadlineExceeded,
+			// lock: &Lock{
+			// 	key:   "key1",
+			// 	value: "value1",
+			// 	client: func() redis.Cmdable {
+			// 		cmd := mocks.NewMockCmdable(ctrl)
+			// 		return cmd
+			// 	}(),
+			// },
+		},
+		{
+			name: "lock not hold",
+			mock: func(ctrl *gomock.Controller) redis.Cmdable {
+				cmd := mocks.NewMockCmdable(ctrl)
+				res := redis.NewCmd(context.Background())
+				res.SetVal(int64(0))
+				cmd.EXPECT().Eval(context.Background(), luaUnlock, []string{"key1"}, []any{"value1"}).
+					Return(res)
+				return cmd
+			},
+			key:     "key1",
+			value:   "value1",
+			wantErr: ErrLockNotHold,
+		},
+		{
+			name: "unlocked",
+			mock: func(ctrl *gomock.Controller) redis.Cmdable {
+				cmd := mocks.NewMockCmdable(ctrl)
+				res := redis.NewCmd(context.Background())
+				res.SetVal(int64(1))
+				cmd.EXPECT().Eval(context.Background(), luaUnlock, []string{"key1"}, []any{"value1"}).
+					Return(res)
+				return cmd
+			},
+			key:   "key1",
+			value: "value1",
+		},
+	}
+	for _, tc := range testCases {
+		t.Run(tc.name, func(t *testing.T) {
+			ctrl := gomock.NewController(t)
+			defer ctrl.Finish()
+			lock := &Lock{
+				key:    tc.key,
+				value:  tc.value,
+				client: tc.mock(ctrl),
+			}
+			err := lock.Unlock(context.Background())
+			assert.Equal(t, tc.wantErr, err)
+		})
+	}
+}
