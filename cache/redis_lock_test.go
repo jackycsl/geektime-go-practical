32ed1bbb4a8e741a7e6accd7f2ffb74a94d084cf
Jacky CSL <jcslmu@gmail.com>
2023-01-20 09:48:29 +0800

    [cache] example error

--- cache/redis_lock_test.go
+++ cache/redis_lock_test.go
@@ -2,6 +2,7 @@ package cache
 
 import (
 	"context"
+	"log"
 	"testing"
 	"time"
 
@@ -224,4 +225,108 @@ func TestLock_Refresh(t *testing.T) {
 			assert.Equal(t, tc.wantErr, err)
 		})
 	}
+}
+
+func ExampleLock_Refresh() {
+	// 加锁成功，你拿到了一个 Lock
+	var l *Lock
+	stopChan := make(chan struct{})
+	errChan := make(chan error)
+	timeoutChan := make(chan struct{}, 1)
+	go func() {
+		// 间隔多久续约一次
+		ticker := time.NewTicker(time.Second * 10)
+		timeoutRetry := 0
+		for {
+			select {
+			case <-ticker.C:
+				// 刷新的超时时间怎么设置
+				ctx, cancel := context.WithTimeout(context.Background(), time.Second)
+				// 出现了 error 了怎么办？
+				err := l.Refresh(ctx)
+				cancel()
+				if err == context.DeadlineExceeded {
+					timeoutChan <- struct{}{}
+					continue // goto timeoutChan
+				}
+				if err != nil {
+					errChan <- err
+					// close(stopChan)
+					// close(errChan)
+					return
+				}
+				timeoutRetry = 0
+			case <-timeoutChan:
+				timeoutRetry++
+				if timeoutRetry > 10 {
+					errChan <- context.DeadlineExceeded
+					return
+				}
+				// 刷新的超时时间怎么设置
+				ctx, cancel := context.WithTimeout(context.Background(), time.Second)
+				// 出现了 error 了怎么办？
+				err := l.Refresh(ctx)
+				cancel()
+				if err == context.DeadlineExceeded {
+					timeoutChan <- struct{}{}
+					continue // goto timeoutChan
+				}
+				if err != nil {
+					errChan <- err
+					// close(stopChan)
+					// close(errChan)
+					return
+				}
+			case <-stopChan:
+				// l.Unlock(context.Background())
+				return
+			}
+		}
+	}()
+
+	// 这边假设就是你的业务
+
+	// 你执行业务，有很复杂的逻辑，要记得在中间步骤检测 errChan 有没有信号
+	// 循环的例子——假如你的业务是循环处理
+	for i := 0; i < 100; i++ {
+		select {
+		// 这里，续约失败
+		case <-errChan:
+			break
+		default:
+			// 正常的业务逻辑
+		}
+	}
+
+	// 如果没有循环，那就是每个步骤内都检测一下
+	select {
+	case err := <-errChan:
+		// 续约失败，你要中断业务
+		log.Fatalln(err)
+		return
+	default:
+		// 这是你的步骤1
+	}
+
+	select {
+	case err := <-errChan:
+		// 续约失败，你要中断业务
+		log.Fatalln(err)
+		return
+	default:
+		// 这是你的步骤2
+	}
+
+	select {
+	case err := <-errChan:
+		// 续约失败，你要中断业务
+		log.Fatalln(err)
+		return
+	default:
+		// 这是你的步骤3
+	}
+
+	// 你的业务结束了，就要退出续约的循环
+	stopChan <- struct{}{}
+	// l.Unlock(context.Background())
 }
