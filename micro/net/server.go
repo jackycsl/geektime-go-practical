167e80b237e1b5bda73511cd1a6794b45e3a83e3
Jacky CSL <jcslmu@gmail.com>
2023-01-30 15:38:42 +0800

    [micro]client & server - header + data

--- micro/net/server.go
+++ micro/net/server.go
@@ -1,10 +1,14 @@
 package net
 
 import (
+	"encoding/binary"
 	"errors"
 	"io"
 	"net"
 )
+
+// 长度字段使用的字节数量
+const numOfLengthBytes = 8
 
 func Serve(network, addr string) error {
 	listener, err := net.Listen(network, addr)
@@ -76,8 +80,82 @@ func handleConnV1(conn net.Conn) error {
 }
 
 func handleMsg(req []byte) []byte {
-	res := make([]byte, len(req))
+	res := make([]byte, 2*len(req))
 	copy(res[:len(req)], req)
 	copy(res[len(req):], req)
 	return res
+}
+
+type Server struct {
+	// network string
+	// addr string
+}
+
+//func NewServer(network, addr string) *Server {
+//	return &Server{
+//		network: network,
+//		addr: addr,
+//	}
+//}
+
+func (s *Server) Start(network, addr string) error {
+	listener, err := net.Listen(network, addr)
+	// 比较常见的就是端口被占用
+	if err != nil {
+		return err
+	}
+
+	for {
+		conn, err := listener.Accept()
+		if err != nil {
+			return err
+		}
+		go func() {
+			if er := s.handleConn(conn); er != nil {
+				conn.Close()
+			}
+		}()
+	}
+}
+
+// 我们可以认为，一个请求包含两部分
+// 1. 长度字段：用八个字节表示
+// 2. 请求数据：
+// 响应也是这个规范
+func (s *Server) handleConn(conn net.Conn) error {
+	for {
+		// lenBs 是长度字段的字节表示
+		lenBs := make([]byte, numOfLengthBytes)
+		_, err := conn.Read(lenBs)
+		if err != nil {
+			return err
+		}
+
+		// 我消息有多长？
+		length := binary.BigEndian.Uint64(lenBs)
+
+		reqBs := make([]byte, length)
+		_, err = conn.Read(reqBs)
+		if err != nil {
+			return err
+		}
+
+		respData := handleMsg(reqBs)
+		respLen := len(respData)
+
+		// 我要在这，构建响应数据
+		// data = respLen 的 64 位表示 + respData
+		res := make([]byte, respLen+numOfLengthBytes)
+		// 第一步：
+		// 把长度写进去前八个字节
+		binary.BigEndian.PutUint64(res[:numOfLengthBytes], uint64(respLen))
+		// 第二步：
+		// 写入数据
+		copy(res[numOfLengthBytes:], respData)
+
+		_, err = conn.Write(res)
+		if err != nil {
+			return err
+		}
+	}
 }
